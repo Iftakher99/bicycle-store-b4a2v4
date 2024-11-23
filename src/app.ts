6ff0e7b21f669e43bbb57ba2e859bcc6d569b982
iftakher99 <19tonmoy99@gmail.com>
2024-11-23 12:47:21 +0600

    initial setup and folder strcture [done]

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+//app routes
+// app.use('/api/');
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Server Running 🏃🏻‍♂️‍➡️');
